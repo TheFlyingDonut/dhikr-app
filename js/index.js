81cc99baea3eff8d89b187851690a0fdbea1ca5d
TheFlyingDonut <TheFlyingDonut@users.noreply.github.com>
2022-02-26 15:43:30 +0000

    adding counter list

--- js/index.js
+++ js/index.js
@@ -3,11 +3,68 @@ const counterNumber = document.querySelector(".counter-number");
 const minusBtn = document.querySelector(".minus-btn");
 const plusBtn = document.querySelector(".plus-btn");
 const refreshIcon = document.querySelector(".fa-redo");
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu");
+const plusIcon = document.querySelector(".plus-icon");
+const addCounterBtn = document.querySelector(".add-counter-btn");
+const addCounterPopUp = document.querySelector(".add-counter-pop-up");
+const addCounterInput = document.querySelector(".add-counter-input");
+const saveBtn = document.querySelector(".save-btn");
 
 refreshIcon.addEventListener("click", () => {
 
     savedCounterNumber = 0;
     counterNumber.innerHTML = 0;
+
+})
+
+menuIcon.addEventListener("click", () => {
+
+    menuIcon.style.backgroundColor = "blue"
+    menu.classList.add("menu-visible");
+
+    if (menu.classList == "menu") {
+
+         menu.classList.remove("menu");   
+         menu.classList.add("menu-visible");
+         
+    } else if (menu.classList == "menu-visible") {
+
+        menu.classList.remove("menu-visible");
+        menu.classList.add("menu");
+
+    }
+
+});
+
+saveBtn.addEventListener("click", () => {
+
+    
+
+})
+
+
+let dhikrCounters = []
+
+let popUpMenu = document.createElement("div");
+popUpMenu.setAttribute("class", "pop-up-menu");
+
+addCounterBtn.appendChild(popUpMenu); 
+
+plusIcon.addEventListener("click", () => {
+
+    if (addCounterPopUp.classList == "add-counter-pop-up") {
+
+        addCounterPopUp.classList.remove("add-counter-pop-up")
+        addCounterPopUp.classList.add("add-counter-pop-up-visible");
+
+    } else if (addCounterPopUp.classList == "add-counter-pop-up-visible") {
+
+        addCounterPopUp.classList.remove("add-counter-pop-up-visible")
+        addCounterPopUp.classList.add("add-counter-pop-up");
+
+    } 
+   
 
 })
 
