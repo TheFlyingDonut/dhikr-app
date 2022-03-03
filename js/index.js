33ada5f61e2ca397f74e558586c5a73e1043f7ff
TheFlyingDonut <TheFlyingDonut@users.noreply.github.com>
2022-03-03 08:58:33 +0000

    various changes
    local storage added for saved counters, various other tweaks

--- js/index.js
+++ js/index.js
@@ -10,6 +10,77 @@ const addCounterBtn = document.querySelector(".add-counter-btn");
 const addCounterPopUp = document.querySelector(".add-counter-pop-up");
 const addCounterInput = document.querySelector(".add-counter-input");
 const saveBtn = document.querySelector(".save-btn");
+var xIcon;
+
+
+let savedCountersArray;
+var savedCountersJson;
+
+// savedCountersJson = JSON.parse(localStorage.getItem("savedCounters"));
+// createCounterList();
+
+
+
+if (localStorage.getItem("firstLaunch") == null) {
+
+    localStorage.setItem("lastCounter", "Alhumdulillah");
+    localStorage.setItem("firstLaunch", false);
+    savedCountersArray = [];
+    savedCountersArray.push("Alhumdulillah", "Subhanallah", "Allahu-Akbar", "Astagfirullah");
+    console.log(savedCountersArray);
+    localStorage.setItem("savedCounters", JSON.stringify(savedCountersArray));
+    savedCountersJson = JSON.parse(localStorage.getItem("savedCounters"));
+    console.log("firstlaunch")
+    createCounterList();
+    
+} else if (localStorage.getItem("firstLaunch") !== null) {
+
+    // savedCountersArray = JSON.parse(localStorage.getItem("savedCounters"));
+    savedCountersJson = JSON.parse(localStorage.getItem("savedCounters"));
+    let lastSavedCounter = localStorage.getItem("lastCounter");
+    dhikrType.innerHTML = lastSavedCounter;
+    createCounterList();
+
+}
+
+function createCounterList() {
+
+    for (var i = 0; i < JSON.parse(localStorage.getItem("savedCounters")).length; i++) {
+
+        var individualCounterDiv = document.createElement("div");
+        individualCounterDiv.setAttribute("class", "individual-counter-div");
+        individualCounterDiv.innerHTML = `<div class="menu-counter-wrap"><span class="menu-counter-name">${JSON.parse(localStorage.getItem("savedCounters"))[i]}</span><span class="xicon" id=${i}>X</span></div>`;
+        addCounterPopUp.appendChild(individualCounterDiv);
+
+        // console.log(individualCounterDiv)
+
+      
+        document.getElementById(i).addEventListener("click", (e) => {
+
+            console.log("X clicked")
+            individualCounterDiv.remove();
+            // JSON.parse(localStorage.getItem("savedCounters")).splice(e.target.id, 1);
+            savedCountersJson.splice(e.target.id, 1);
+            console.log(savedCountersJson)
+            localStorage.setItem("savedCounters", JSON.stringify(savedCountersJson));
+            console.log(savedCountersJson);
+
+        })
+        
+
+    }
+
+    individualCounterDiv.addEventListener("click", (e) => {
+
+        console.log(e.target)
+    
+    
+    });
+
+
+    
+
+}
 
 refreshIcon.addEventListener("click", () => {
 
@@ -20,7 +91,6 @@ refreshIcon.addEventListener("click", () => {
 
 menuIcon.addEventListener("click", () => {
 
-    menuIcon.style.backgroundColor = "blue"
     menu.classList.add("menu-visible");
 
     if (menu.classList == "menu") {
@@ -35,53 +105,60 @@ menuIcon.addEventListener("click", () => {
 
     }
 
-});
-
-saveBtn.addEventListener("click", () => {
-
-    
-
-})
-
-
-let dhikrCounters = []
-
-let popUpMenu = document.createElement("div");
-popUpMenu.setAttribute("class", "pop-up-menu");
-
-addCounterBtn.appendChild(popUpMenu); 
-
-plusIcon.addEventListener("click", () => {
-
     if (addCounterPopUp.classList == "add-counter-pop-up") {
 
-        addCounterPopUp.classList.remove("add-counter-pop-up")
+        addCounterPopUp.classList.remove("add-counter-pop-up");
         addCounterPopUp.classList.add("add-counter-pop-up-visible");
 
     } else if (addCounterPopUp.classList == "add-counter-pop-up-visible") {
 
-        addCounterPopUp.classList.remove("add-counter-pop-up-visible")
+        addCounterPopUp.classList.remove("add-counter-pop-up-visible");       
         addCounterPopUp.classList.add("add-counter-pop-up");
 
     } 
+
+});
+
+saveBtn.addEventListener("click", () => {
+
+    let individualCounterDiv = document.createElement("div");
+    individualCounterDiv.innerHTML = `<div class="menu-counter-wrap"><span class="menu-counter-name">${addCounterInput.value}</span><span class="x-icon">X</span></div>`;
+    individualCounterDiv.setAttribute("class", "individual-counter-div");
+    addCounterPopUp.appendChild(individualCounterDiv);
+    savedCountersJson.push(addCounterInput.value);
+    addCounterInput.value = "";
+    console.log(savedCountersJson)
+    localStorage.setItem("savedCounters", JSON.stringify(savedCountersJson));
+
+    document.querySelectorAll(".individual-counter-div").forEach(item => {
+
+        item.remove();
+
+    })
+
+    createCounterList();
+
+
+});
+
+let popUpMenu = document.createElement("div");
+popUpMenu.setAttribute("class", "pop-up-menu");
+
+
+
+// addCounterBtn.appendChild(popUpMenu); 
+
+// plusIcon.addEventListener("click", () => {
+
+
    
 
-})
+// })
 
-let currentDhikrType;
+// let currentDhikrType;
 let savedCounterNumber;
 
-if (localStorage.getItem("savedDhikrType") !== null) {
-
-    localStorage.getItem("savedDhikrType");
-
-} else {
-
-    currentDhikrType = "";
-
-}
-
-dhikrType.innerHTML = currentDhikrType;
+// dhikrType.innerHTML = currentDhikrType;
 
 if (localStorage.getItem("savedCounterNumber") !== null) {
 
@@ -115,6 +192,6 @@ plusBtn.addEventListener("click", () => {
 
     counterNumber.innerHTML = savedCounterNumber+=1;
     console.log("Plus")
-    localStorage.setItem("savedCounterNumber", savedCounterNumber);
+    localStorage.setItem("savedCounterNumber", JSON.stringify(savedCounterNumber));
 
 });
